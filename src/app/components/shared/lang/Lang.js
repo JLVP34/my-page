594040493fbf52e3a0b9306050c9d39b16bfd73a
Juan Luis Vazquez <juan.vazquez@edreamsodigeo.com>
2020-05-11 18:02:52 +0200

    Added modal to select language

--- src/app/components/shared/lang/Lang.js
+++ src/app/components/shared/lang/Lang.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Lang.css';
 
-const Lang = () => (
-    <div className="language">
-        <div className="flag">
-            <img alt="Español" src="images/flags/gb.svg"/>
+const Lang = () => {
+    const [lang, setLang] = useState('gb');
+    const [shown, setShown] = useState(false);
+
+    const onClickFlag = () => {
+        if (!shown) setShown(true);
+    }
+
+    const onSelectLang = (newLang) => {
+        if (lang !== newLang) setLang(newLang); 
+        if (shown) setShown(false);
+    }
+
+    return (
+        <div className="language">
+            <div className="flag" onClick={onClickFlag}>
+                <img alt="English" src={`images/flags/${lang}.svg`}/>
+            </div>
+            <div className={`modal ${shown ? 'shown' : ''}`}></div>
+            <div className={`selector ${shown ? 'shown' : ''}`}>
+                <div className="item" onClick={() => onSelectLang('gb')}>
+                    <img className="flag-icon" alt="English" src="images/flags/gb.svg"/> 
+                    <span>English</span>
+                </div>
+                <hr></hr>
+                <div className="item" onClick={() => onSelectLang('es')}>
+                    <img className="flag-icon" alt="Español" src="images/flags/es.svg"/>
+                    <span>Español</span>
+                </div>
+            </div>
         </div>
-    </div>
-);
+    )
+};
 
 export default Lang;
