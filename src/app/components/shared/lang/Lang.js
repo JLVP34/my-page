a0c773cb30098a507b0fbd489371236d5f356a99
Juan Luis Vazquez <juan.vazquez@edreamsodigeo.com>
2020-05-12 18:47:14 +0200

    Refactor lang selector

--- src/app/components/shared/lang/Lang.js
+++ src/app/components/shared/lang/Lang.js
@@ -2,35 +2,46 @@ import React, { useState } from 'react';
 
 import './Lang.css';
 
+const languages = [
+    {id: 'gb', name: 'English'},
+    {id: 'es', name: 'Español'}
+];
+
 const Lang = () => {
     const [lang, setLang] = useState('gb');
     const [shown, setShown] = useState(false);
 
     const onClickFlag = () => {
         if (!shown) setShown(true);
-    }
+    };
+
+    const onHideShown = () => {
+        if (shown) setShown(false);
+    };
 
     const onSelectLang = (newLang) => {
         if (lang !== newLang) setLang(newLang); 
-        if (shown) setShown(false);
-    }
+        onHideShown();
+    };
+
+    const buildLangItem = ({id, name}, hr) => (
+        <div key={id}>
+            <div className="item" onClick={() => onSelectLang(id)}>
+                <img className="flag-icon" alt={name} src={`images/flags/${id}.svg`}/> 
+                <span>{name}</span>
+            </div>
+            {hr ? <hr></hr> : null}
+        </div>
+    );
 
     return (
         <div className="language">
             <div className="flag" onClick={onClickFlag}>
                 <img alt="English" src={`images/flags/${lang}.svg`}/>
             </div>
-            <div className={`modal ${shown ? 'shown' : ''}`}></div>
+            <div className={`modal ${shown ? 'shown' : ''}`} onClick={onHideShown}></div>
             <div className={`selector ${shown ? 'shown' : ''}`}>
-                <div className="item" onClick={() => onSelectLang('gb')}>
-                    <img className="flag-icon" alt="English" src="images/flags/gb.svg"/> 
-                    <span>English</span>
-                </div>
-                <hr></hr>
-                <div className="item" onClick={() => onSelectLang('es')}>
-                    <img className="flag-icon" alt="Español" src="images/flags/es.svg"/>
-                    <span>Español</span>
-                </div>
+                {languages.map((lang, i) => buildLangItem(lang, languages.length !== i+1))}
             </div>
         </div>
     )
